c634052bd43388fbde76d71da9c9a8398e9f1fd6
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-17 14:58:37 +0400

    Added Category class with validation using Data Annotations

---
+++ LibraryManagementSystem/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryID { get; set; } // Primary Key with auto-increment
+
+        [Required]
+        [StringLength(255, ErrorMessage = "Category name cannot exceed 255 characters.")]
+        public string CName { get; set; } // Name of the category
+
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Number of books cannot be negative.")]
+        public int NumberOfBooks { get; set; } // Total number of books in the category
+    }
+}
