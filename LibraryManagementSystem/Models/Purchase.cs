f52785dcd218a804a930915663c59503cc0e8d75
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-17 15:58:51 +0400

    Add   Purchase Table ,and Purchase Details Tables

---
+++ LibraryManagementSystem/Models/Purchase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Purchase
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PurchaseID { get; set; } // Primary Key
+
+        [Required]
+        public DateTime PurchaseDate { get; set; } // Date of purchase
+
+        [Required]
+        [ForeignKey("User")]
+        public int UserID { get; set; } // Foreign Key referencing User
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalPrice { get; set; } // Total price of the purchase
+
+        // Navigation property
+        public User User { get; set; }
+    }
+}
---
+++ LibraryManagementSystem/Models/PurchaseDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+
+namespace LibraryManagementSystem.Models
+{
+    [PrimaryKey(nameof(PurchaseID), nameof(BookID))]
+    public class PurchaseDetails
+    {
+        [Required]
+        [ForeignKey("Purchase")]
+        public int PurchaseID { get; set; } // Foreign Key referencing Purchase
+
+        [Required]
+        [ForeignKey("Book")]
+        public int BookID { get; set; } // Foreign Key referencing Book
+
+        [Required]
+        public int Quantity { get; set; } // Quantity of the book in the purchase
+
+        // Navigation properties
+        public Purchase Purchase { get; set; }
+        public Book Book { get; set; }
+    }
+}
