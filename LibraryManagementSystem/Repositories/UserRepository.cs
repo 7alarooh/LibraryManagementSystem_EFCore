f668c2b8b00105e5afa26ca407bf8939fb32585d
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-18 09:45:24 +0400

    Create the Specific Repositories

---
+++ LibraryManagementSystem/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Repositories
+{
+    public class UserRepository : Repository<User>
+    {
+        public UserRepository(ApplicationDbContext context) : base(context) { }
+
+        public int CountByGender(Gender gender)
+        {
+            return _context.Users.Count(u => u.Gender == gender);
+        }
+    }
+}
