5c15ec26f08b694a8bd5f0237a9af6622b924e84
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-18 09:46:37 +0400

    BookRepository

---
+++ LibraryManagementSystem/Repositories/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using LibraryManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Repositories
+{
+    public class BookRepository : Repository<Book>
+    {
+        public BookRepository(ApplicationDbContext context) : base(context) { }
+
+        public decimal GetTotalPrice()
+        {
+            return _context.Books.Sum(book => book.CopyPrice * book.TotalCopies);
+        }
+
+        public decimal GetMaxPrice()
+        {
+            return _context.Books.Max(book => book.CopyPrice);
+        }
+
+        public int GetTotalBorrowedBooks()
+        {
+            return _context.Books.Sum(book => book.Borrowed);
+        }
+
+        public int GetTotalBooksPerCategoryName(string categoryName)
+        {
+            return _context.Books.Where(book => book.Category.CName == categoryName)
+                                 .Sum(book => book.TotalCopies);
+        }
+    }
+}
