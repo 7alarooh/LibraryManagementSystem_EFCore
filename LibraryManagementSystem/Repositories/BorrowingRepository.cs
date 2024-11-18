bd84ed760c7a7f04cd40169f50d0a680e2ed88c0
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-18 09:49:12 +0400

    BorrowingRepository

---
+++ LibraryManagementSystem/Repositories/BorrowingRepository.cs
@@ -0,0 +1,14 @@
+﻿using LibraryManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Repositories
+{
+    public class BorrowingRepository : Repository<Borrowing>
+    {
+        public BorrowingRepository(ApplicationDbContext context) : base(context) { }
+    }
+}
