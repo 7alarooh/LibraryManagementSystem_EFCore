9d7cd0b1a279cb057b267e4e996abc4e76acc731
Azzak <azzaalmahrooqi79@gmail.com>
2024-11-18 09:50:16 +0400

    PurchaseRepository

---
+++ LibraryManagementSystem/Repositories/PurchaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using LibraryManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Repositories
+{
+    public class PurchaseRepository : Repository<Purchase>
+    {
+        public PurchaseRepository(ApplicationDbContext context) : base(context) { }
+    }
+}
